4fd9267cf35eede27c0c4a685855a430d25860bd
izumi-t-yamana <t-yamana@izmc.co.jp>
2024-05-17 07:28:29 +0000

    env: pptomlをDEV限定インストール設定

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,5 +4,6 @@
 pip install -U pip
 
 # NOTE: requirements.txt ベースから pyproject.tomlベースへ変更
+# 参考: https://stackoverflow.com/a/77134290
 # pip install -r requirements.txt
-pip install .
+pip install '.[dev]'
