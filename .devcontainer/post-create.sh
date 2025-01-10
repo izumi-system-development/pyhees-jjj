49e99201e591eebd6a96e952fd31a3db845a0008
izumi-t-yamana <t-yamana@izmc.co.jp>
2025-01-10 05:29:14 +0000

    env: poetry.lock は共有すべきだった

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,5 +9,8 @@ pip install poetry
 # プロジェクトのルートに仮想環境を作成
 poetry config virtualenvs.in-project true
 
+# NOTE: poetry lock を実行しないで下さい
+# バージョン固定ファイルが再生成され、開発者間でのバージョンの不一致が発生するため
+
 # 開発用の依存関係をインストール
 poetry install --with dev
