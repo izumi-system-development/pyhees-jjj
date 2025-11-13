d6d9f95bcac9f2f4dda2c5b9f146da6ee49c2b79
izumi-t-yamana <t-yamana@izmc.co.jp>
2025-11-13 04:27:23 +0000

    debug

--- src/tests/carryover_heat/test_get_C.py
+++ src/tests/carryover_heat/test_get_C.py
@@ -14,17 +14,15 @@ def test_熱容量取得_居室_標準住戸(exp_C_BR_i):
     """ 居室の面積が標準住戸通りであれば、熱容量も標準住戸通りとなる
     """
     # Arrange
-    A_HCZ_i = np.array([ld.get_A_HCZ_R_i[i] for i in range(5)]).reshape(-1, 1)  # (5,) -> (5,1)
-
+    A_HCZ_i = np.array([ld.get_A_HCZ_R_i(i) for i in range(5)])  \
+                .reshape(-1, 1)  # (5,) -> (5,1)
     # Act
     C_BR_i = jjj_carryover_heat.get_C_BR_i(A_HCZ_i)
-
     # Assert
     exp_C_BR_i = np.array(exp_C_BR_i) \
                     .reshape(-1,1)  # (5,) -> (5,1)
     assert np.all(C_BR_i == exp_C_BR_i),  \
         "熱容量が標準住戸通りでない"
-
 
 @pytest.mark.parametrize(["exp_C_NR"], [
         pytest.param(jjj_consts.C1_NR_R),  # 熱容量1(空間・什器のみ)
@@ -34,7 +32,6 @@ def test_熱容量取得_非居室_標準住戸(exp_C_NR):
     """
     # Arrange & Act
     C_NR = jjj_carryover_heat.get_C_NR(jjj_consts.A_NR_R)
-
     # Assert
-    assert exp_C_NR == C_NR, \
+    assert exp_C_NR == C_NR,  \
         "熱容量が標準住戸通りでない"
